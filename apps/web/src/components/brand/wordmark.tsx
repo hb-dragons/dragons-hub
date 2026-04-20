c137182756c4c6d17694ff5c018b2b0f306ada7d
James Neumann <jannesneemann@gmail.com>
2026-04-20 15:53:16 +0200

    feat(web): add Wordmark brand component

---
+++ apps/web/src/components/brand/wordmark.tsx
@@ -0,0 +1,19 @@
+const ASPECT = 1432 / 384;
+
+type WordmarkProps = {
+  width?: number;
+  alt?: string;
+  className?: string;
+};
+
+export function Wordmark({ width = 220, alt = "Dragons", className }: WordmarkProps) {
+  return (
+    <img
+      src="/brand/wordmark.svg"
+      width={width}
+      height={width / ASPECT}
+      alt={alt}
+      className={className}
+    />
+  );
+}
